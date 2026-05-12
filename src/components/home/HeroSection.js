91d4f10918a747fe31712475230c07d06ebea52a
Maarij Ali <maarijali29@gmail.com>
2026-05-13 04:49:36 +0500

    chatbot icon and email me

--- src/components/home/HeroSection.js
+++ src/components/home/HeroSection.js
@@ -55,7 +55,7 @@ const HeroSection = ({
             rel="noopener noreferrer"
             aria-label={`Compose email in Gmail to ${HERO_EMAIL}`}
           >
-            Email Us
+            Email me
           </a>
         </div>
 
